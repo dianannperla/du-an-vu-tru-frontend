d3298a0ecaca8205740fef49f5f478b2c55e2893
dianannperla <waafirmelyza@hotmail.com>
2025-09-30 15:51:30 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chào mừng bạn đến với Dự Án Vũ Trụ</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
